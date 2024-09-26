de044de8c7e0bdb6fd33ef21c33709f0345c7611
Rakesh-608 <rakeshchenna2002@gmail.com>
2024-09-27 02:02:07 +0530

    authentication added

--- src/main/java/com/example/alert_service/config/AlertConfig.java
+++ src/main/java/com/example/alert_service/config/AlertConfig.java
@@ -1,4 +1,4 @@
-package com.example.alert_service.congig;
+package com.example.alert_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/example/alert_service/controller/WeatherAlertController.java
+++ src/main/java/com/example/alert_service/controller/WeatherAlertController.java
@@ -1,6 +1,9 @@
 package com.example.alert_service.controller;
 
+import com.example.alert_service.converter.Converter;
+import com.example.alert_service.domain.Alert;
 import com.example.alert_service.domain.WeatherAlert;
+import com.example.alert_service.dto.AlertDto;
 import com.example.alert_service.service.WeatherAlertService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,31 +12,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 // https://api.weatherbit.io/v2.0/alerts?lat=49.8209&lon=18.2625&key=9b048f06cc0449a095e8bfc3067db3aa
 // http://localhost:8080/fetch-alerts?latitude=49.8209&longitude=18.2625
 
 @RestController
 public class WeatherAlertController {
-    @Autowired
-    private WeatherAlertService weatherAlertService;
 
-//    @Value("${other.service.url}")
-//    private String otherServiceUrl;
-//
-//    private final RestTemplate restTemplate;
-//
-//    @Autowired
-//    public WeatherAlertController(RestTemplate restTemplate) {
-//        this.restTemplate = restTemplate;
-//    }
-//
-//    @GetMapping("/fetch-alerts")
-//    public String fetchAndSendAlerts(@RequestParam String location) {
-//        WeatherAlert alerts = weatherAlertService.getWeatherAlerts(location);
-//        // Send alerts to another microservice
-//        String response = restTemplate.postForObject(otherServiceUrl, alerts, String.class);
-//        return response;
-//    }
+    private final WeatherAlertService weatherAlertService;
+
+    private final Converter converter;
+
+    public WeatherAlertController(WeatherAlertService weatherAlertService, Converter converter) {
+        this.weatherAlertService = weatherAlertService;
+        this.converter = converter;
+    }
 
 
     @GetMapping("/fetch-alerts")
@@ -41,4 +35,14 @@ public class WeatherAlertController {
         WeatherAlert alerts = weatherAlertService.getWeatherAlerts(latitude, longitude);
         return alerts; // Return the retrieved alerts
     }
+
+    @GetMapping("/getWeatherAlerts")
+    public List<AlertDto> getWeatherAlerts(@RequestParam Double latitude, @RequestParam Double longitude){
+        List<Alert> alertsList=converter.extractAlertsFromWeatherAlert(weatherAlertService.getWeatherAlerts(latitude, longitude));
+        return converter.convertToAlertDtoList(alertsList);
+    }
+
+
+
+
 }
